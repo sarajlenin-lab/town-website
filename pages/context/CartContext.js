13ce0b8799eee5ffde001b3bb527653c9b519416
sarajlenin-lab <sarajlenin@gmail.com>
2026-03-26 22:19:05 +0400

    Update CartContext.js

--- pages/context/CartContext.js
+++ pages/context/CartContext.js
@@ -1,16 +1,13 @@
-import { createContext, useState } from "react";
+// context/CartContext.js
+import { createContext, useState } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, setCart }}>
       {children}
     </CartContext.Provider>
   );
