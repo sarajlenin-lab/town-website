a789c3f0f7813e83cbb1cd3c6a126d5c91a2ff76
sarajlenin-lab <sarajlenin@gmail.com>
2026-03-26 22:30:59 +0400

    Update cart.js

--- pages/pages/pages/cart.js
+++ pages/pages/pages/cart.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from '../../../context/CartContext';
 
 export default function Cart() {
   const { cart } = useContext(CartContext);
